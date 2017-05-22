38231fc57751f5b82cfd104d471540d2889ef06b
Git-WiedyTP <wiedytirapratama@gmail.com>
2017-05-22 09:45:14 +0700

    messages

---
+++ index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <!-- Basic Page Needs
+        ================================================== -->
+        <meta charset="utf-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <link rel="icon" type="image/png" href="images/favicon.png">
+        <title>Simulasi OwnCloud</title>
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="author" content="">
+        <!-- Mobile Specific Metas
+        ================================================== -->
+        <meta name="format-detection" content="telephone=no">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Template CSS Files
+        ================================================== -->
+        <!-- Twitter Bootstrs CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <!-- Ionicons Fonts Css -->
+        <link rel="stylesheet" href="css/ionicons.min.css">
+        <!-- animate css -->
+        <link rel="stylesheet" href="css/animate.css">
+        <!-- Hero area slider css-->
+        <link rel="stylesheet" href="css/slider.css">
+        <!-- owl craousel css -->
+        <link rel="stylesheet" href="css/owl.carousel.css">
+        <link rel="stylesheet" href="css/owl.theme.css">
+        <link rel="stylesheet" href="css/jquery.fancybox.css">
+        <!-- template main css file -->
+        <link rel="stylesheet" href="css/main.css">
+        <!-- responsive css -->
+        <link rel="stylesheet" href="css/responsive.css">
+
+        <!-- Template Javascript Files
+        ================================================== -->
+        <!-- modernizr js -->
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+        <!-- jquery -->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <!-- owl carouserl js -->
+        <script src="js/owl.carousel.min.js"></script>
+        <!-- bootstrap js -->
+
+        <script src="js/bootstrap.min.js"></script>
+        <!-- wow js -->
+        <script src="js/wow.min.js"></script>
+        <!-- slider js -->
+        <script src="js/slider.js"></script>
+        <script src="js/jquery.fancybox.js"></script>
+        <!-- template main js -->
+        <script src="js/main.js"></script>
+    </head>
+    <body>
+	 <!--
+        ==================================================
+        Header Section Start
+        ================================================== -->
+        <header id="top-bar" class="navbar-fixed-top animated-header">
+            <div class="container">
+                <div class="navbar-header">
+                    <!-- responsive nav button -->
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    </button>
+                    <!-- /responsive nav button -->
+
+                    <!-- logo -->
+                    <div class="navbar-brand">
+                        <a href="index.html" >
+                            <img src="images/logo-telkom.png" alt="">
+                        </a>
+                    </div>
+                    <!-- /logo -->
+                </div>
+                <!-- main menu -->
+                <nav class="collapse navbar-collapse navbar-right" role="navigation">
+                    <div class="main-menu">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li>
+
+                            </li>
+                            <li></li>
+                            <li></li>
+                            <li class="dropdown">
+
+                            </li>
+                            <li class="dropdown">
+
+                            </li>
+                            <li><a href="#"></a></li>
+                        </ul>
+                    </div>
+                </nav>
+                <!-- /main nav -->
+            </div>
+        </header>
+
+        <section class="global-page-header">
+            <div class="container">
+                <div class="row">
+
+                </div>
+            </div>
+        </section><!--/#page-header-->
+
+<section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="block">
+                        <center>
+                            <h2 class="subtitle">Aplikasi Simulasi Integrasi OwnCloud</h2>
+                        </center>
+                        <p class="subtitle-des">
+
+                        </p>
+                        <div class="form-group">
+                            <div class="form-group ">
+                                <form method="POST" action="aksi.php" enctype="multipart/form-data">
+                                    <div class="col-lg-12">
+                                        <div class="col-lg-8">
+                                            <div class="form-group">
+                                                <input type="file" placeholder="Search" class="form-control" name="file" id="file"/>
+                                            </div>
+                                        </div>
+										<div class="col-lg-1">
+                                            <!--<button id="simpan" class="btn btn-primary" type="button"><i class="ion-ios-plus"></i></button>-->
+                                            <input type="submit" name="upload" value="Upload" class="btn btn-primary"></input>
+                                        </div>
+                                        </div>
+                                 </form>
+                                        </div>
+                                        <div class="col-lg-12">
+										<div class="col-lg-4">
+                                            <label></label>
+                                        </div></div>
+                                    </div>
+
+                           <!--<div class="col-lg-12">
+                                <button id="simpan" class="btn btn-primary" type="button">Simpan</button>
+
+                            </div>
+                               </form>-->
+                            </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+    </section>
+
+		<footer id="footer">
+            <div class="container">
+                <div class="col-md-8">
+                    <p class="copyright">Copyright: <span>2017</span> . Telkom Japati</p>
+                </div>
+
+            </div>
+        </footer> <!-- /#footer -->
+
+    </body>
+</html>
---
+++ koneksi/koneksi.php
@@ -0,0 +1,21 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oc8c0fd71e03',
+  'passwordsalt' => '515a13302a6b3950a9d0fdb970191a',
+  'trusted_domains' =>
+  array (
+    0 => 'localhost',
+    1 => 'wiedytpoc',
+    2 => '192.168.43.65'
+  ),
+  'datadirectory' => '/var/www/owncloud/data',
+  'dbtype' => 'mysql',
+   'version' => '9.1.4',
+  'dbname' => 'dbowncloud',
+  'dbhost' => 'localhost',
+  'dbuser' => 'admin',
+  'dbpassword' => 'root',
+  'installed' => true,
+
+
+);
